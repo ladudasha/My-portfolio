440ee8318adaca76e24b60e8bd8ce753e0914ed7
ladudasha <darya.gameza@gmail.com>
2021-12-10 23:59:50 +0300

    10/12/21

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,39 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+// var $div = $('skills'), //Блок, доскроллив до которого анимация должна стартовать.
+//     inited = false;
+
+// $(window).scroll(function() {
+//     if (inited) return;
+
+//     var $t = $(this),
+//         s_top = $t.scrollTop();
+
+//     if (s_top + $t.height() + 50 < $div.offset().top)
+//         return;
+
+//     /*
+//      * Тут твой код.
+//      * Еще можно весь твой код затолкать в функцию, а здесь её просто вызвать.
+//      * Разницы нет.
+//      */
+
+//     inited = true
+// });
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.skills__inner');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
