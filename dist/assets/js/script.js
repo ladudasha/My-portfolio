b0c5d897a6160a1ecb14f5d3e8f812ff857359eb
ladudasha <darya.gameza@gmail.com>
2022-01-14 23:53:01 +0300

    form phpmailer 14/01

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -6,23 +6,23 @@ var swiper = new Swiper(".slider-my", {
   });
 
 
-function onEntry(entry) {
-    entry.forEach(change => {
-        if (change.isIntersecting) {
-            change.target.classList.add('element-show');
-        }
-    });
-}
+// function onEntry(entry) {
+//     entry.forEach(change => {
+//         if (change.isIntersecting) {
+//             change.target.classList.add('element-show');
+//         }
+//     });
+// }
 
-let options = {
-    threshold: [0.5]
-};
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.skills__inner');
+// let options = {
+//     threshold: [0.5]
+// };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.skills__inner');
 
-for (let elm of elements) {
-    observer.observe(elm);
-}
+// for (let elm of elements) {
+//     observer.observe(elm);
+// }
 
 
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,22 +6,22 @@ var swiper = new Swiper(".slider-my", {
   });
 
 
-function onEntry(entry) {
-    entry.forEach(change => {
-        if (change.isIntersecting) {
-            change.target.classList.add('element-show');
-        }
-    });
-}
+// function onEntry(entry) {
+//     entry.forEach(change => {
+//         if (change.isIntersecting) {
+//             change.target.classList.add('element-show');
+//         }
+//     });
+// }
 
-let options = {
-    threshold: [0.5]
-};
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.skills__inner');
+// let options = {
+//     threshold: [0.5]
+// };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.skills__inner');
 
-for (let elm of elements) {
-    observer.observe(elm);
-}
+// for (let elm of elements) {
+//     observer.observe(elm);
+// }
 
 
