c176e3ce8335c2d4e4c8975e4ac36abac7a04d3d
ladudasha <darya.gameza@gmail.com>
2022-01-15 23:45:19 +0300

    form post messages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,6 +6,25 @@ var swiper = new Swiper(".slider-my", {
   });
 
 
+
+  let anchors = document.querySelectorAll("a[href*='#']")
+
+  for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault()
+
+      let blockID  = anchor.getAttribute('href');
+      document.querySelector(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+
+      })
+
+    })
+
+  }
+
+
 // function onEntry(entry) {
 //     entry.forEach(change => {
 //         if (change.isIntersecting) {
