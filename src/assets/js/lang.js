fd691f24c04c7b11cee7c5993e79d038a30cdb4c
ladudasha <darya.gameza@gmail.com>
2022-02-18 22:26:06 +0300

    lang.js 18.02

--- src/assets/js/lang.js
+++ src/assets/js/lang.js
@@ -1,49 +1,222 @@
 const langArr = {
-    "test" : {
+    "test": {
         "ru": "Веб разработчик из Минска. Иногда работаю фрилансером",
         "en": "Web developer from Minsk. Sometimes works as a freelancer."
 
     },
-    "title-site" :  {
+    "title-site": {
         "ru": "ПортфолиоДарья",
         "en": "PortfolioDarya",
     },
-    "home" :  {
+    "home": {
         "ru": "ДОМОЙ",
         "en": "HOME",
-    }, 
-    "portfolio" :  {
+    },
+    "portfolio": {
         "ru": "ПОРТФОЛИО",
         "en": "PORTFOLIO",
-    }, 
-    "skills" :  {
+    },
+    "skills": {
         "ru": "НАВЫКИ",
         "en": "SKILLS",
-    }, 
-    "aboutMe" :  {
+    },
+    "aboutMe": {
         "ru": "ОБО МНЕ",
         "en": "ABOUT ME",
-    }, 
-    "contacts" :  {
+    },
+    "contacts": {
         "ru": "КОНТАКТЫ",
         "en": "CONTACT",
-    }, 
-    "letter" :  {
+    },
+    "letter": {
         "ru": "Д",
         "en": "D",
-    }, 
-    "home__title" :  {
+    },
+    "home__title": {
         "ru": "арья Ладудо",
         "en": "arya Ladudo",
-    }, 
-    "contacts" :  {
+    },
+    "contacts": {
         "ru": "КОНТАКТЫ",
         "en": "CONTACT",
-    }, 
-    "contacts" :  {
+    },
+    "contacts": {
         "ru": "КОНТАКТЫ",
         "en": "CONTACT",
-    }, 
-    
+    },
+
+    // SECTION PORTFOLIO
+
+    "title__portfolio": {
+        "ru": "ПОРТФОЛИО",
+        "en": "PORTFOLIO",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    // block1
+
+    "project__one": {
+        "ru": "Проект 1",
+        "en": "Project 1",
+    },
+    "block1__view": {
+        "ru": "Посмотреть проект Helicopter flight",
+        "en": "View project Helicopter flight",
+    },
+    "portfolio__onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Layout of a one-page site",
+    },
+    "portfolio__bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+
+    // block2
+    "project__two": {
+        "ru": "Проект 2",
+        "en": "Project 2",
+    },
+    "block2__view": {
+        "ru": "Посмотреть проект Shop-catalog",
+        "en": "View project Shop-catalog",
+    },
+    "portfolio__group": {
+        "ru": "Групповая работа",
+        "en": "Group work",
+    },
+    "portfolio__multi": {
+        "ru": "многостраничный сайт",
+        "en": "multi-page site",
+    },
+    // block3
+    "project__three": {
+        "ru": "Проект 3",
+        "en": "Project 3",
+    },
+    "block3__view": {
+        "ru": "Посмотреть проект Productivities",
+        "en": "View project Productivities",
+    },
+    "portfolio__onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Layout of a one-page site",
+    },
+
+
+
+
+    // block4
+    "project__four": {
+        "ru": "Проект 4",
+        "en": "Project 4",
+    },
+    "block4__view": {
+        "ru": "Посмотреть проект ThriveTalk",
+        "en": "View project ThriveTalk",
+    },
+
+
+
+
+
+
+    // block5
+    "project__five": {
+        "ru": "Проект 5",
+        "en": "Project 5",
+    },
+    "block5__view": {
+        "ru": "Посмотреть проект Explore channels",
+        "en": "View project Explore channels",
+    },
+
+
+
+
+    // block6
+    "project__six": {
+        "ru": "Проект 6",
+        "en": "Project 6",
+    },
+    "block6__view": {
+        "ru": "Посмотреть проект Cards",
+        "en": "View project Cards",
+    },
+
+
+    // block7
+    "project__seven": {
+        "ru": "Проект 7 для WordPress",
+        "en": "Project 7 Project for WordPress",
+    },
+    "block7__view": {
+        "ru": "Посмотреть проект Archmove",
+        "en": "View project Archmove",
+    },
+
+
+
+
+    // block8
+    "project__eight": {
+        "ru": "Проект 8",
+        "en": "Project 8",
+    },
+    "block8__view": {
+        "ru": "Посмотреть проект Smoothies",
+        "en": "View project Smoothies",
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    "subtitle__portfolio": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+
 
 }
