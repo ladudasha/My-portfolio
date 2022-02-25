3a768d92b0e5326d55917e3d2dc915cae1a22aba
ladudasha <darya.gameza@gmail.com>
2022-02-25 14:06:12 +0300

    Update 2022-02-25T11:06:07.890Z

--- assets/js/script.js
+++ assets/js/script.js
@@ -1046,6 +1046,330 @@ var VanillaTilt = (function () {
     return VanillaTilt;
     
     }());
+const langArr = {
+    "h1": {
+        "ru": "Веб разработчик из Минска.",
+        "en": "Web developer from Minsk."
+
+    },
+    "title-site": {
+        "ru": "ПортфолиоДарья",
+        "en": "PortfolioDarya",
+    },
+    "home": {
+        "ru": "ДОМОЙ",
+        "en": "HOME",
+    },
+    "portfolio": {
+        "ru": "ПОРТФОЛИО",
+        "en": "PORTFOLIO",
+    },
+    "skills": {
+        "ru": "НАВЫКИ",
+        "en": "SKILLS",
+    },
+    "aboutMe": {
+        "ru": "ОБО МНЕ",
+        "en": "ABOUT ME",
+    },
+    "contacts": {
+        "ru": "КОНТАКТЫ",
+        "en": "CONTACT",
+    },
+    // burger-menu
+    "burger-home": {
+        "ru": "ДОМОЙ",
+        "en": "HOME",
+    },
+    "burger-portfolio": {
+        "ru": "ПОРТФОЛИО",
+        "en": "PORTFOLIO",
+    },
+    "burger-skills": {
+        "ru": "НАВЫКИ",
+        "en": "SKILLS",
+    },
+    "burger-aboutMe": {
+        "ru": "ОБО МНЕ",
+        "en": "ABOUT ME",
+    },
+    "burger-contacts": {
+        "ru": "КОНТАКТЫ",
+        "en": "CONTACT",
+    },
+    "letter": {
+        "ru": "Д",
+        "en": "D",
+    },
+    "home-title": {
+        "ru": "арья Ладудо",
+        "en": "arya Ladudo",
+    },
+    "contacts": {
+        "ru": "КОНТАКТЫ",
+        "en": "CONTACT",
+    },
+    "contacts": {
+        "ru": "КОНТАКТЫ",
+        "en": "CONTACT",
+    },
+
+    // SECTION PORTFOLIO
+
+    "portfolio-title": {
+        "ru": "ПОРТФОЛИО",
+        "en": "PORTFOLIO",
+    },
+    "portfolio-subtitle": {
+        "ru": "ПРОЕКТЫ",
+        "en": "SELECTED WORKS",
+    },
+    // block1
+
+    "project-one": {
+        "ru": "Проект 1",
+        "en": "Project 1",
+    },
+    "block1-view": {
+        "ru": "Посмотреть проект 'Helicopter flight'",
+        "en": "View project 'Helicopter flight'",
+    },
+    "proj1-onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Layout of a one-page site",
+    },
+    "proj1-bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+    "proj1-responsive": {
+        "ru": "Отзывчивый",
+        "en": "Responsive",
+    },
+    
+
+    // block2
+    "project-two": {
+        "ru": "Проект 2",
+        "en": "Project 2",
+    },
+    "block2-view": {
+        "ru": "Посмотреть проект 'Shop-catalog'",
+        "en": "View project 'Shop-catalog'",
+    },
+    "proj2-group": {
+        "ru": "Групповая работа",
+        "en": "Group work",
+    },
+    "proj2-multi": {
+        "ru": "многостраничный сайт",
+        "en": "multi-page site",
+    },
+
+    // block3
+    "project-three": {
+        "ru": "Проект 3",
+        "en": "Project 3",
+    },
+    "block3-view": {
+        "ru": "Посмотреть проект 'Productivities'",
+        "en": "View project 'Productivities'",
+    },
+    "proj3-onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Layout of a one-page site",
+    },
+    "proj3-bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+    "proj3-responsive": {
+        "ru": "Отзывчивый",
+        "en": "Responsive",
+    },
+
+
+
+
+    // block4
+    "project-four": {
+        "ru": "Проект 4",
+        "en": "Project 4",
+    },
+    "block4-view": {
+        "ru": "Посмотреть проект 'ThriveTalk'",
+        "en": "View project 'ThriveTalk'",
+    },
+    "proj4-onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Development of a one-page site",
+    },
+    "proj4-bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+    "proj4-responsive": {
+        "ru": "Отзывчивый",
+        "en": "Responsive",
+    },
+
+    // block5
+    "project-five": {
+        "ru": "Проект 5",
+        "en": "Project 5",
+    },
+    "block5-view": {
+        "ru": "Посмотреть проект 'Explore channels'",
+        "en": "View project 'Explore channels'",
+    },
+    "proj5-desctop": {
+        "ru": "Desktop версия",
+        "en": "Desktop development",
+    },
+    "proj5-component": {
+        "ru": "компонентный подход",
+        "en": "component approach",
+    },
+
+    // block6
+    "project-six": {
+        "ru": "Проект 6",
+        "en": "Project 6",
+    },
+    "block6-view": {
+        "ru": "Посмотреть проект 'Cards'",
+        "en": "View project 'Cards'",
+    },
+    "proj6-multiPage": {
+        "ru": "Разработка многостраничного сайта",
+        "en": "Layout of a multi-page site",
+    },
+    "proj6-group": {
+        "ru": "Групповая работа",
+        "en": "Group work",
+    },
+    "proj6-bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+
+
+    // block7
+    "project-seven": {
+        "ru": "Проект 7 для WordPress",
+        "en": "Project 7 Project for WordPress",
+    },
+    "block7-view": {
+        "ru": "Посмотреть проект 'Archmove'",
+        "en": "View project 'Archmove'",
+    },
+    "proj7-onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Layout of a one-page site",
+    },
+    "proj7-responsive": {
+        "ru": "Отзывчивый",
+        "en": "Responsive",
+    },
+
+
+    // block8
+    "project-eight": {
+        "ru": "Проект 8",
+        "en": "Project 8",
+    },
+    "block8-view": {
+        "ru": "Посмотреть проект 'Smoothies'",
+        "en": "View project Smoothies",
+    },
+    "proj8-onePage": {
+        "ru": "Разработка одностраничного сайта",
+        "en": "Development of a one-page site",
+    },
+    "proj8-bem": {
+        "ru": "БЭМ",
+        "en": "BEM",
+    },
+       // portfolio BTN
+    "portfolio-btn": {
+        "ru": "Показать больше",
+        "en": "View more",
+    },
+
+
+    // skills
+
+    "skills-title": {
+        "ru": "НАВЫКИ",
+        "en": "SKILLS",
+    },
+    "skills-title": {
+        "ru": "НАВЫКИ",
+        "en": "SKILLS",
+    },
+    "programmingSkills": {
+        "ru": "НАВЫКИ ПРОГРАММИРОВАНИЯ",
+        "en": "PROGRAMMING SKILLS",
+    },
+    "skills-text": {
+        "ru": "Моя основная область знаний — HTML CSS. В своей работе я использую такие программы, как Gulp, Sass, React,Material-UI, Bootstrap и другие программы. Использую графитовые редакторы Figma, Photoshop. Делаю адаптивную, кроссбраузерную, смысловую, валидную верстку. Mobile first, Pixel perfect, создание анимации. Я продолжаю учиться каждый день, приобретая новые навыки, поднимая свой уровень. В будущем я хочу продолжить изучение Java Script и двигаться в направлении Front-end разработки.",
+        "en": "My main area of ​​expertise is HTML CSS.In my work I use programs such as Gulp, Sass, React, Bootstrap and others. I use graphite editors Figma, Photoshop.I make adaptive, crossbrowser, semantic, valid layout. Mobile first, Pixel perfect, animation creation. I continue to learn every day, acquiring new skills, raising my level. In the future I want to continue learning Java Script and move towards Front-end development.",
+    },
+
+    // about me
+
+    "about-title": {
+        "ru": "ОБО МНЕ",
+        "en": "ABOUT ME",
+    },
+    "about-text": {
+        "ru": "Здравствуйте, меня зовут Дарья, я HTML CSS разработчик. Проживаю и ищу работу в Минске. Моя основная область знаний — HTML CSS/SCSS. В своей работе я использую такие программы, как Gulp, Sass, React,Material-UI, Bootstrap и другие программы. Ищу постоянную работу в творческой компании с интересными задачами и дружным коллективом! Мне нравится работать с новыми людьми.Буду рада стать частью вашей команды. Работала в команде и занимался групповыми проектами.  Несколько лет назад я решила координально поменять профессию и уйти 'со сцены' в IT. Остаюсь творческим человеком, в свободное время преподаю балет, воспитываю 2 сыновей. Обладаю такими личными качествами как",
+        "en": "Hello, my name is Darya, I'm an HTML CSS developer. I live and look for work in Minsk, Republic of Belarus. My main area of ​​expertise is HTML CSS/SCSS. In my work I use programs such as Gulp, Sass, React, Bootstrap and others. I am looking for a permanent job in a creative company with interesting tasks and a friendly team! I like work with new people. I would be happy to be part of your team. Worked in a team and was engaged in group projects. I have such personal qualities as",
+    },
+    "slide1": {
+        "ru": "Умение эффективно распоряжаться временем",
+        "en": "Ability to manage time effectively",
+    },
+    "slide2": {
+        "ru": "Внимание к деталям",
+        "en": "Attention to detail",
+    },
+    "slide3": {
+        "ru": "Хорошо обучаема",
+        "en": "Well trained",
+    },
+    "slide4": {
+        "ru": "Организована",
+        "en": "Organized",
+    },
+
+    // contacts
+
+    "contacts-title": {
+        "ru": "КОНТАКТЫ",
+        "en": "CONTACTS",
+    },
+    "contacts-subtitle": {
+        "ru": "Связаться",
+        "en": "Get in Touch",
+    },
+    "contacts-text": {
+        "ru": "Меня интересует работа веб-разработчика — особенно интересные и креативные проекты. Однако, если у вас есть другой запрос или вопрос, пожалуйста, заполните форму в этом разделе.",
+        "en": "I am interested in the work of a web developer - especially interesting and creative projects. However, if you have another request or question, please fill out the form in this section.",
+    },
+    "address": {
+        "ru": "Беларусь, Минск",
+        "en": "Belarus, Minsk",
+    },
+    "form-btn": {
+        "ru": "Отправить",
+        "en": "Submit",
+    },
+
+
+
+}
+
 var swiper = new Swiper(".slider-my", {
   navigation: {
     nextEl: ".swiper-button-next",
@@ -1101,41 +1425,6 @@ window.addEventListener("scroll", function () {
 })
 
 
-// печатная машинка
-if(document.querySelector('.home')) {
-  const text = [
-    // 'Darya Ladudo\n',
-    'Web developer from Minsk. Sometimes works as a freelancer.\n',
-  ];
-  
-  let line = 0;
-    let count = 0;
-    let result = '';
-    function typeLine() {
-      let interval = setTimeout(
-        () => {
-          result += text[line][count]
-          document.querySelector('p').innerHTML =result +'|';
-  
-  
-        count++;
-        if (count >= text[line].length) {
-          count = 0;
-          line++;
-          if (line == text.length) {
-            clearTimeout(interval);
-            document.querySelector('p').innerHTML =result;
-            return true;
-          }
-        }
-        typeLine();
-      }, getRandomInt(getRandomInt(250*2.5)))
-    }
-    typeLine();
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-}
 
 
 
@@ -1175,7 +1464,7 @@ toggleBurgerMenu()
 
 
 
-// GALLERY
+// // GALLERY
 
 const gallery = document.querySelectorAll(".portfolio--hide");
 const showMore = document.querySelector(".portfolio__link-more");
@@ -1199,11 +1488,96 @@ function showMoreBtn() {
       showMore.removeAttribute('href')
     },1000)
   } 
+
     showMoreText.classList.toggle('btn--show');
-    showMoreText.innerHTML = (showMoreText.className == 'portfolio__link-text btn--show') ? 'Hide projects ' : 'View more';
+    if(showMoreText.classList.contains('btn--show')) {
+      showMoreText.innerHTML = hash === 'en'? 
+      'Hide projects': 
+      'Скрыть проекты'
+    } 
+    else {
+      showMoreText.innerHTML =  hash === 'en' ?
+      'View more':
+      'Показать больше'
+    }
+  }
+
+
+
+
+// мультиязычность
+const select = document.querySelector('select');
+const allLang = ['en', 'ru'];
+
+select.addEventListener('change', changeURLLaguage);
+// перенаправить на url с указанием языка
+function changeURLLaguage(){
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+let hash;
+function changeLanguage() {
+  hash = window.location.hash;
+  hash = hash.substr(1);
+
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+
+  }
+  select.value = hash;
+
+  document.querySelector('title').innerHTML = langArr['title-site'][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector('.lng-' + key);
+    // let elem = document.querySelector(`.${key}--lng`);
+    if (elem) {
+        elem.innerHTML = langArr[key][hash];
+    }
+
+}
   
 }
+changeLanguage();
 
+
+
+
+// печатная машинка
+
+  const text = [
+    // 'Web developer from Minsk.\n',
+    langArr["h1"][hash]
+  ];
+  
+  let line = 0;
+    let count = 0;
+    let result = '';
+    function typeLine() {
+      let interval = setTimeout(
+        () => {
+          result += text[line][count]
+          document.querySelector('h1').innerHTML =result +'|';
+  
+  
+        count++;
+        if (count >= text[line].length) {
+          count = 0;
+          line++;
+          if (line == text.length) {
+            clearTimeout(interval);
+            document.querySelector('h1').innerHTML =result;
+            return true;
+          }
+        }
+        typeLine();
+      }, getRandomInt(getRandomInt(250*2.5)))
+    }
+    typeLine();
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
 
 
 //# sourceMappingURL=script.js.map
